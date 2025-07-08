f581225f9635be79f0dc54a4895e06be07c8da34
Chad Kouse <chad.kouse@gmail.com>
2025-07-08 00:43:39 -0400

    split application into htpc and smarthome

--- update-manifests.sh
+++ update-manifests.sh
@@ -3,8 +3,8 @@
 SRCROOT="$( CDPATH='' cd -- "$(dirname "$0")" && pwd -P )"
 AUTOGENMSG="# This is an auto-generated file. DO NOT EDIT"
 
-echo "${AUTOGENMSG}" > "${SRCROOT}/install_x86_64.yaml"
-kustomize build "${SRCROOT}/overlays/x86" >> "${SRCROOT}/install_x86_64.yaml"
+echo "${AUTOGENMSG}" > "${SRCROOT}/install_htpc.yaml"
+kustomize build "${SRCROOT}/overlays/htpc" >> "${SRCROOT}/install_htpc.yaml"
 
 echo "${AUTOGENMSG}" > "${SRCROOT}/install_argocd.yaml"
 kustomize build "${SRCROOT}/argocd" >> "${SRCROOT}/install_argocd.yaml"
