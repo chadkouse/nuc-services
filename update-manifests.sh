c2de25d46e3202f6078f9d513754170b1c308906
Chad Kouse <chad.kouse@gmail.com>
2025-07-15 18:00:39 -0400

    Try to add matrix application

--- update-manifests.sh
+++ update-manifests.sh
@@ -9,5 +9,8 @@ kustomize build "${SRCROOT}/overlays/htpc" >> "${SRCROOT}/install_htpc.yaml"
 echo "${AUTOGENMSG}" > "${SRCROOT}/install_smarthome.yaml"
 kustomize build "${SRCROOT}/overlays/smarthome" >> "${SRCROOT}/install_smarthome.yaml"
 
+echo "${AUTOGENMSG}" > "${SRCROOT}/install_matrix.yaml"
+kustomize build "${SRCROOT}/overlays/matrix" >> "${SRCROOT}/install_matrix.yaml"
+
 echo "${AUTOGENMSG}" > "${SRCROOT}/install_argocd.yaml"
 kustomize build "${SRCROOT}/argocd" >> "${SRCROOT}/install_argocd.yaml"
